6c784fc57f01e2026c766b2c77062f93ff46a3a9
lilizhiwei <1548790965@qq.com>
2017-09-29 11:59:55 +0800

    上传代码

--- guest/guest/urls.py
+++ guest/guest/urls.py
@@ -19,5 +19,7 @@ from sign import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^index/$', views.index),
+    url(r'^login/$', views.login),
+    url(r'^login_action/$', views.login_action),
+    url(r'^event_manage/$', views.event_manage),
 ]
--- guest/sign/views.py
+++ guest/sign/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 
 # Create your views here.
 
-def index(request):
-	return render(request,"shizhan.html")
+def login(request):
+	return render(request,"login.html")
 
+def event_manage(request):
+	return render(request,"event_manage.html")
+
+def login_action(request):
+	if request.method == 'POST':
+		username = request.POST.get('username','')
+		password = request.POST.get('password','')
+		if username == '123' and password == '123456':
+			return HttpResponseRedirect('/event_manage')
+		else:
+			return render(request,'login.html',{'error':'username or password error!'})
