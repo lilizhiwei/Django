16af8a87b865ee3afb3c79d335eb2250f3510972
lilizhiwei <1548790965@qq.com>
2017-09-29 14:53:08 +0800

    上传代码

--- guest/sign/models.py
+++ guest/sign/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+# 发布会表
+class Event(models.Model):
+	name = models.CharField(max_length=100)
+	limit = modles.IntegerField()
+	status = modles.BooleanFiled()
+	address = modles.CharField(max_length=200)
+	start_time = modles.DateTimeField('events time')
+	creatr_time = modles.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.name
+
+#嘉宾表
+class Guest(models.Model):
+	
+
+		
+
+		
--- guest/sign/views.py
+++ guest/sign/views.py
@@ -1,19 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+# 账号admin 邮箱admin@mail.com 密码li123456789
 
 def login(request):
 	return render(request,"login.html")
-
-def event_manage(request):
-	return render(request,"event_manage.html")
 
 def login_action(request):
 	if request.method == 'POST':
 		username = request.POST.get('username','')
 		password = request.POST.get('password','')
-		if username == '123' and password == '123456':
-			return HttpResponseRedirect('/event_manage')
+		user = auth.authenticate(username=username,password=password)
+		if user is not None:
+			auth.login(request,user) #登录
+			request.session['user']=username #添加浏览器cookie
+			response = HttpResponseRedirect('/event_manage')
+			return response
 		else:
 			return render(request,'login.html',{'error':'username or password error!'})
+
+@login_required
+def event_manage(request):
+	username = request.session.get('user','') #读取浏览器cookie
+	return render(request,"event_manage.html",{"user":username})
